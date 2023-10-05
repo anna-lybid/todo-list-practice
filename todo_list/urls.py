48463b8be2c9d08f12b5ae18e38fa6fc6e30d7b1
anna-lybid <anna.lybid@gmail.com>
2023-10-05 11:49:12 +0300

    Create base list page, add some data to db

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from todo_list.views import index
+from todo_list.views import index, TagListView
 
 urlpatterns = [
-    path("", index, name="todo-list-index"),
+    path("", index, name="todo-index"),
+    path("tags/", TagListView.as_view(), name="tags"),
 ]
--- todo_list/views.py
+++ todo_list/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
+
+from todo_list.models import Tag
 
 
 def index(request):
     return render(request, "todo_list/index.html")
+
+
+class TagListView(generic.ListView):
+    model = Tag
