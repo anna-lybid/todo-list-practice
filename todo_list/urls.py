e3a015dd78b18831c75b619775b668228d13f010
anna-lybid <anna.lybid@gmail.com>
2023-10-05 13:12:35 +0300

    Add routing for delete/update

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
-from todo_list.views import index, TagListView
+from todo_list.views import index, TagListView, TagUpdateView, TagDeleteView
 
 urlpatterns = [
-    path("", index, name="todo-index"),
-    path("tags/", TagListView.as_view(), name="tags"),
+    path("", index, name="index"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete")
 ]
+
+
+app_name = "todo_list"
--- todo_list/views.py
+++ todo_list/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
 from todo_list.models import Tag
@@ -10,3 +11,13 @@ def index(request):
 
 class TagListView(generic.ListView):
     model = Tag
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = ["name"]
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("todo_list:tag-list")
